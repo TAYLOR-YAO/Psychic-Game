8607712002137bdd05e6ff9f35b185cf4f6cfba2
TAYLOR YAO <yaotaylor25@gmail.com>
2018-03-02 19:56:58 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 
     
-     var computerPic = ["s", "g", "k"];
+     var computerPic = ["a","b","c","e","f","g", "h", "i","j","k","l","m","n", "o","p", "q", "r","s","t","u","v","w","x","y", "z"];
      var wins = 0;
      var losses = 0;
      var leftGuessings = 9;
      var computerGuess = computerPic;
     
      var computerGuess = computerPic[Math.floor(Math.random() * computerPic.length)];
-     console.log(computerGuess);
+     //console.log(computerGuess);
  
     
      document.onkeyup = function(event) {
@@ -22,13 +22,13 @@
                 computerGuess = computerPic[Math.floor(Math.random() * computerPic.length)];
                 document.getElementById("guessSoFar").innerText = " ";
                // docCurrentGuess.innerText = wins;
-               console.log(computerGuess);
+               //console.log(computerGuess);
                 // console.log(wins);
              }else{
                  leftGuessings--;
                  document.getElementById("guessesLeft").innerText = leftGuessings;
                 //console.log(leftGuessings);
-                console.log(computerGuess);
+                //console.log(computerGuess);
 
              }
              if (leftGuessings === 0){
@@ -38,7 +38,12 @@
                  losses++;
                  document.getElementById("badGuess").innerText = losses;
                  document.getElementById("guessSoFar").innerText = " ";
-                 console.log(computerGuess);
+                 //console.log(computerGuess);
+             }
+             if(wins==9){
+                 alert("Good job, You Are A Genius!")
+             }else if( losses==3){
+                 alert("Game Over!............ start a new Game.");
              }
              
          }
